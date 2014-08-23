1c2a5f4b03f7cf1e71b889308cba6945d3c5080a
Ryan Phillips <ryan.phillips@rackspace.com>
2014-08-22 17:51:21 -0700

    modularize

--- gosec.go
+++ gosec.go
@@ -20,13 +20,10 @@ import (
 	"code.google.com/p/gopass"
 )
 
-var secringPath = "~/.gnupg/secring.gpg"
-var prompt = "password: "
+var DefaultSecureRingPath = "~/.gnupg/secring.gpg"
+var DefaultPrompt = "password: "
 
 func main() {
-	var regex *regexp.Regexp
-	var err error
-
 	directoryRootPtr := flag.String("s", "", "Directory")
 	grepStringPtr := flag.String("g", "", "Regex String")
 	recipientEmailPtr := flag.String("r", "", "Recipient Email")
@@ -44,30 +41,81 @@ func main() {
 		os.Exit(1)
 	}
 
-	if *grepStringPtr != "" {
-		regex, err = regexp.Compile(*grepStringPtr)
-		if err != nil {
-			log.Fatal(err)
-		}
+	ctx := NewSecureContext(
+		DefaultSecureRingPath,
+		*recipientEmailPtr,
+		*directoryRootPtr,
+	)
+
+	err := ctx.ReadKeyRing()
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	secringPath, _ = expandPath(secringPath)
+	_, err = ctx.GetPassword()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	ctx.FindRegex(*grepStringPtr)
+}
+
+type SecureContext struct {
+	SecureRingPath string
+	EmailRecipient string
+	DirectoryRoot  string
+
+	PrivateRing openpgp.EntityList
+	Password    string
+
+	SearchRegex *regexp.Regexp
+}
+
+func NewSecureContext(secureRingPath, emailRecipient, directoryRoot string) *SecureContext {
+	return &SecureContext{
+		SecureRingPath: secureRingPath,
+		EmailRecipient: emailRecipient,
+		DirectoryRoot:  directoryRoot,
+	}
+}
+
+func (ctx *SecureContext) GetPassword() (string, error) {
+	var err error
+	ctx.Password, err = gopass.GetPass(DefaultPrompt)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	return ctx.Password, nil
+}
+
+func (ctx *SecureContext) ReadKeyRing() error {
+	secringPath, _ := expandPath(ctx.SecureRingPath)
 	privringFile, err := os.Open(secringPath)
 	if err != nil {
-		log.Fatal(err)
-		return
+		return err
+	}
+	defer privringFile.Close()
+
+	ctx.PrivateRing, err = openpgp.ReadKeyRing(privringFile)
+	if err != nil {
+		return err
 	}
 
-	privring, err := openpgp.ReadKeyRing(privringFile)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+	return nil
+}
 
-	password, err := gopass.GetPass(prompt)
-	if err != nil {
-		log.Fatal(err)
-		return
+func (ctx *SecureContext) FindRegex(regexStr string) error {
+	var regex *regexp.Regexp
+	var err error
+
+	if len(regexStr) > 0 {
+		regex, err = regexp.Compile(regexStr)
+		if err != nil {
+			return err
+		}
 	}
 
 	fileCallback := func(path string, fi os.FileInfo, err error) error {
@@ -77,28 +125,92 @@ func main() {
 		if fi.IsDir() {
 			return nil
 		}
-		if filepath.Ext(fi.Name()) == ".gpg" {
-			return decryptFile(*recipientEmailPtr, password, path, regex, privring)
+		if filepath.Ext(fi.Name()) != ".gpg" {
+			return nil
 		}
+
+		md, err := ctx.DecryptFile(path, regex)
+		if err != nil {
+			return err
+		}
+
+		if regex != nil {
+			first := true
+			lineNumber := 0
+			foundMatch := false
+
+			scanner := bufio.NewScanner(md.UnverifiedBody)
+			for scanner.Scan() {
+				lineNumber++
+				line := scanner.Text()
+				if regex.Match([]byte(line)) {
+					foundMatch = true
+					if first {
+						fmt.Println(path)
+						first = false
+					}
+					fmt.Printf("%v:%v\n", lineNumber, line)
+				}
+			}
+
+			if foundMatch {
+				fmt.Println()
+			}
+		} else {
+			io.Copy(os.Stdout, md.UnverifiedBody)
+		}
+
 		return nil
 	}
 
-	filesPath := path.Join(*directoryRootPtr, "files")
-	err = filepath.Walk(filesPath, fileCallback)
-	if err != nil {
-		log.Fatal(err)
+	filesPath := path.Join(ctx.DirectoryRoot, "files")
+	if err = filepath.Walk(filesPath, fileCallback); err != nil {
+		return err
 	}
+	return nil
 }
 
-func getKeyByEmail(keyring openpgp.EntityList, email string) *openpgp.Entity {
-	for _, entity := range keyring {
+func (ctx *SecureContext) GetKeyByEmail() *openpgp.Entity {
+	for _, entity := range ctx.PrivateRing {
 		for _, ident := range entity.Identities {
-			if ident.UserId.Email == email {
+			if ident.UserId.Email == ctx.EmailRecipient {
 				return entity
 			}
 		}
 	}
 	return nil
+}
+
+func (ctx *SecureContext) DecryptFile(filePath string, regex *regexp.Regexp) (*openpgp.MessageDetails, error) {
+	secfile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer secfile.Close()
+
+	block, err := armor.Decode(secfile)
+	if err != nil {
+		return nil, err
+	}
+
+	recipientEntity := ctx.GetKeyByEmail()
+	if recipientEntity == nil {
+		return nil, errors.New("Invalid Recipient")
+	}
+
+	ents := openpgp.EntityList([]*openpgp.Entity{recipientEntity})
+
+	promptCallback := func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
+		for _, k := range keys {
+			err := k.PrivateKey.Decrypt([]byte(ctx.Password))
+			if err == nil {
+				return nil, nil
+			}
+		}
+		return nil, errors.New("invalid password or no private key")
+	}
+
+	return openpgp.ReadMessage(block.Body, ents, promptCallback, nil)
 }
 
 func expandPath(p string) (string, error) {
@@ -113,67 +225,6 @@ func expandPath(p string) (string, error) {
 		p = strings.Replace(p, "~", usr.HomeDir, 1)
 	}
 	return p, nil
-}
-
-func decryptFile(recipient, password, filePath string, regex *regexp.Regexp, privring openpgp.EntityList) error {
-	secfile, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-
-	block, err := armor.Decode(secfile)
-	if err != nil {
-		return err
-	}
-
-	recipientEntity := getKeyByEmail(privring, recipient)
-	if recipientEntity == nil {
-		return errors.New("Invalid Recipient")
-	}
-
-	ents := openpgp.EntityList([]*openpgp.Entity{recipientEntity})
-
-	promptCallback := func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
-		for _, k := range keys {
-			err := k.PrivateKey.Decrypt([]byte(password))
-			if err == nil {
-				return nil, nil
-			}
-		}
-		return nil, errors.New("invalid password or no private key")
-	}
-
-	md, err := openpgp.ReadMessage(block.Body, ents, promptCallback, nil)
-	if err != nil {
-		return err
-	}
-
-	if regex != nil {
-		first := true
-		lineNumber := 0
-		foundMatch := false
-
-		scanner := bufio.NewScanner(md.UnverifiedBody)
-		for scanner.Scan() {
-			lineNumber++
-			line := scanner.Text()
-			if regex.Match([]byte(line)) {
-				foundMatch = true
-				if first {
-					fmt.Println(filePath)
-					first = false
-				}
-				fmt.Printf("%v:%v\n", lineNumber, line)
-			}
-		}
-
-		if foundMatch {
-			fmt.Println()
-		}
-	} else {
-		io.Copy(os.Stdout, md.UnverifiedBody)
-	}
-	return nil
 }
 
 var Usage = func() {
